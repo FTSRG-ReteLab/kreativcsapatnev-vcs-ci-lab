30694f1f6abd0d66475b6c28272121fa825f3d45
meres <danyx000@gmail.com>
2020-03-09 15:52:22 +0100

    train controller travis should throw error

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -12,7 +12,7 @@ public class TrainControllerImpl implements TrainController {
 	public void followSpeed() {
 		if (referenceSpeed < 0) {
 			referenceSpeed = 0;
-		} else {
+		} els {
 		    if(referenceSpeed+step > 0) {
                 referenceSpeed += step;
             } else {
