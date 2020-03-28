ea8ff99c78d63f4cb0d79fc2cda0a66283cd7b5b
SillingerMester <SillingerMester@github.com>
2020-03-28 03:07:23 +0100

    Implemented changing speed with timer.

--- train-system/src/main/java/hu/bme/mit/train/system/TrainSystem.java
+++ train-system/src/main/java/hu/bme/mit/train/system/TrainSystem.java
@@ -7,11 +7,19 @@ import hu.bme.mit.train.interfaces.TrainUser;
 import hu.bme.mit.train.sensor.TrainSensorImpl;
 import hu.bme.mit.train.user.TrainUserImpl;
 
+import java.util.Timer;
+
 public class TrainSystem {
 
 	private TrainController controller = new TrainControllerImpl();
 	private TrainUser user = new TrainUserImpl(controller);
 	private TrainSensor sensor = new TrainSensorImpl(controller, user);
+	
+	private Timer timer = new Timer();
+	
+	public TrainSystem() {
+		timer.scheduleAtFixedRate(controller.followSpeed, 100, 100);
+	}
 
 	public TrainController getController() {
 		return controller;
